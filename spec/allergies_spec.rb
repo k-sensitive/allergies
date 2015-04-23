5650a406b0bcf666a64146ab0a8a3200771400ad
Kim Dang & Dan Minor <student@epicodus.com>
2015-04-23 15:00:54 -0700

    wrote a spec for determining if you're allergic to cats or pollen

--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -5,4 +5,7 @@ describe('Fixnum#allergic') do
   it('determine if you are allergic to cats') do
     expect(128.allergic()).to(eq('cats'))
   end
+  it("determines if you're allergic to cats or pollen") do
+    expect(192.allergic()).to(eq('cats pollen'))
+  end
 end
