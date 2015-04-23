dd572af0cedbcb75046a6b81422fcf2221297fab
Kim Dang & Dan Minor <student@epicodus.com>
2015-04-23 15:32:31 -0700

    updated comments

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -17,37 +17,6 @@ class Fixnum
       iterator /= 2
       pass += 1
     end
-    # if al_score.>=(128)
-    #   allergies.push("cats")
-    #   al_score -= 128
-    # end
-    # if al_score.>=(64)
-    #   allergies.push("pollen")
-    #   al_score -= 64
-    # end
-    # if al_score.>=(32)
-    #   allergies.push("chocolate")
-    #   al_score -= 32
-    # end
-    # if al_score.>=(16)
-    #   allergies.push("tomatoes")
-    #   al_score -= 16
-    # end
-    # if al_score.>=(8)
-    #   allergies.push("strawberries")
-    #   al_score -= 8
-    # end
-    # if al_score.>=(4)
-    #   allergies.push("shellfish")
-    #   al_score -= 4
-    # end
-    # if al_score.>=(2)
-    #   allergies.push("peanuts")
-    #   al_score -= 2
-    # end
-    # if al_score.==(1)
-    #   allergies.push("eggs")
-    # end
     allergies.join(" ")
   end
 end
