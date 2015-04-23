05b323371eaba94b0a59ff82e97e4dc2f5f7bb4f
Kim Dang & Dan Minor <student@epicodus.com>
2015-04-23 15:17:20 -0700

    passed spec for checking allergy score

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -10,6 +10,29 @@ class Fixnum
       allergies.push("pollen")
       al_score -= 64
     end
+    if al_score.>=(32)
+      allergies.push("chocolate")
+      al_score -= 32
+    end
+    if al_score.>=(16)
+      allergies.push("tomatoes")
+      al_score -= 16
+    end
+    if al_score.>=(8)
+      allergies.push("strawberries")
+      al_score -= 8
+    end
+    if al_score.>=(4)
+      allergies.push("shellfish")
+      al_score -= 4
+    end
+    if al_score.>=(2)
+      allergies.push("peanuts")
+      al_score -= 2
+    end
+    if al_score.==(1)
+      allergies.push("eggs")
+    end
     allergies.join(" ")
   end
 end
