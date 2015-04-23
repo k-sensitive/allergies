5482a84ac05571bf3254d374259a425505b133f5
Kim Dang & Dan Minor <student@epicodus.com>
2015-04-23 15:10:02 -0700

    Wrote a spec to check an allergy score for what allergy information it provides

--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -2,10 +2,13 @@ require('rspec')
 require('allergies')
 
 describe('Fixnum#allergic') do
-  it('determine if you are allergic to cats') do
+  it("determine if you are allergic to cats") do
     expect(128.allergic()).to(eq('cats'))
   end
   it("determines if you're allergic to cats or pollen") do
     expect(192.allergic()).to(eq('cats pollen'))
+  end
+  it("determines what you're allergic to given a score") do
+    expect(211.allergic()).to(eq('cats pollen tomatoes peanuts eggs'))
   end
 end
