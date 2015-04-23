15f8e3c2fba9fb8f9c2944f612405aadae774c71
Kim Dang & Dan Minor <student@epicodus.com>
2015-04-23 15:06:19 -0700

    Pass cat and pollen allergy test.

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -1,7 +1,15 @@
 class Fixnum
   define_method(:allergic) do
-    if self.==128
-      "cats"
+    allergies = []
+    al_score = self
+    if al_score.>=(128)
+      allergies.push("cats")
+      al_score -= 128
     end
+    if al_score.>=(64)
+      allergies.push("pollen")
+      al_score -= 64
+    end
+    allergies.join(" ")
   end
 end
